ded1e42c80f6019a06cf5ccd2104b584a594725c
Patrick O'Neill <synapse.and.syntax@gmail.com>
2011-02-08 17:03:27 -0500

    fixed critical typo in rules regexp

--- splicerack.py
+++ splicerack.py
@@ -2,7 +2,7 @@
 import re, sys, os
 from nfa import *
 print sys.argv
-filename = "ex84.hs"
+filename = "examples/baaplus.head"
 if __name__ == '__main__':
     print __name__
     if len(sys.argv) > 1:
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         text = "".join(f.readlines())
     symmetric = eval(re.findall(r"symmetric ([0-1])",text)[0])
     reflexive = 1 if re.findall(r"type ([a-z-])",text)[0] == "head" else 0
-    rules = re.findall(r"([a-z1]+,[a-z1]+;[a-z1]+,[a-z1])+",text)
+    rules = re.findall(r"([a-z1]+,[a-z1]+;[a-z1]+,[a-z1]+)",text)
     axioms = re.findall(r"\[axioms\](.*)",text,re.DOTALL)[0].split()
     test = nfa(rules,axioms,reflexive,symmetric)
     f, ext = os.path.splitext(filename)
