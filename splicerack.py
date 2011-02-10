cb4c9ae1585c41d7c16497d4d4ef91df4733a2a1
Patrick O'Neill <synapse.and.syntax@gmail.com>
2011-02-10 12:19:27 -0500

    moved input file parsing into reader.py

--- splicerack.py
+++ splicerack.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python
 import re, sys, os
+from reader import *
 from nfa import *
-print sys.argv
-filename = "examples/baaplus.head"
-
-def parse_head_file(filename):
-    with open(filename) as f:
-        text = "".join(f.readlines())
-    symmetric = eval(re.findall(r"symmetric ([0-1])",text)[0])
-    reflexive = 1 if re.findall(r"type ([a-z-])",text)[0] == "head" else 0
-    rules = re.findall(r"([a-z1]+,[a-z1]+;[a-z1]+,[a-z1]+)",text)
-    axioms = re.findall(r"\[axioms\](.*)",text,re.DOTALL)[0].split()
-    return (rules, axioms, reflexive, symmetric)
+filename = "baaplus.head"
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         filename = sys.argv[1]
-
     test = nfa(*parse_head_file(filename))
     f, ext = os.path.splitext(filename)
     test.graphviz(f)
