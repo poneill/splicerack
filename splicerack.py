58588404d4d023e6c4b1a29a7f5e68eb9d8eecd3
Patrick O'Neill <synapse.and.syntax@gmail.com>
2011-02-10 11:55:29 -0500

    factored head file parsing out of main block

--- splicerack.py
+++ splicerack.py
@@ -3,17 +3,21 @@ import re, sys, os
 from nfa import *
 print sys.argv
 filename = "examples/baaplus.head"
-if __name__ == '__main__':
-    print __name__
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
+
+def parse_head_file(filename):
     with open(filename) as f:
         text = "".join(f.readlines())
     symmetric = eval(re.findall(r"symmetric ([0-1])",text)[0])
     reflexive = 1 if re.findall(r"type ([a-z-])",text)[0] == "head" else 0
     rules = re.findall(r"([a-z1]+,[a-z1]+;[a-z1]+,[a-z1]+)",text)
     axioms = re.findall(r"\[axioms\](.*)",text,re.DOTALL)[0].split()
-    test = nfa(rules,axioms,reflexive,symmetric)
+    return (rules, axioms, reflexive, symmetric)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+
+    test = nfa(*parse_head_file(filename))
     f, ext = os.path.splitext(filename)
     test.graphviz(f)
 
